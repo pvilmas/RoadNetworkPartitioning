1395f103e26d2b1b38ac5965d069b6886283bc3e
Lulu1234 <lulu.koudel@centrum.cz>
2023-10-22 17:57:36 +0200

    DFS for PUNCH

--- Punch/src/alg/punchPart/PunchAlgorithm.java
+++ Punch/src/alg/punchPart/PunchAlgorithm.java
@@ -1,12 +1,8 @@
 package alg.punchPart;
 
-import bp.roadnetworkpartitioning.Graph;
-import bp.roadnetworkpartitioning.GraphPartition;
-import bp.roadnetworkpartitioning.IPartitioning;
-import bp.roadnetworkpartitioning.Vertex;
+import bp.roadnetworkpartitioning.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Class with PUNCH algorithm implementation.
@@ -34,6 +30,54 @@ public class PunchAlgorithm implements IPartitioning {
     private void detectTinyCuts(){
 
     }
+    //TODO
+    private void DFS(Vertex s) {
+        List<Vertex> visited = new ArrayList<>();
+        List<Vertex> unvisited = new ArrayList<>();
+
+        Stack<Vertex> stack = new Stack<>();
+        stack.push(s);
+        Map<Vertex, List<Vertex>> adj = createAdjacencyLists();
+        while(!stack.empty()) {
+            s = stack.peek();
+            stack.pop();
+            if(!visited.contains(s)) {
+                System.out.print(s + " ");
+                visited.add(s);
+            }
+            Iterator<Vertex> itr = adj.get(s).iterator();
+            while (itr.hasNext()) {
+                Vertex v = itr.next();
+                if(!visited.contains(v)) {
+                    stack.push(v);
+                }
+            }
+
+        }
+    }
+
+    private Map<Vertex, List<Vertex>> createAdjacencyLists() {
+        Map<Vertex, List<Vertex>> adjacencyLists = new HashMap<>();
+        for(Vertex vertex: graph.getVertices().values()){
+            List<Vertex> neighbours = new ArrayList<>();
+            List<Edge> startingEdges = vertex.getStartingEdges();
+            for(Edge edge: startingEdges){
+                Vertex v = edge.getEndpoint();
+                if(!neighbours.contains(v)){
+                    neighbours.add(v);
+                }
+            }
+            List<Edge> endingEdges = vertex.getEndingEdges();
+            for(Edge edge: endingEdges){
+                Vertex v = edge.getStartpoint();
+                if(!neighbours.contains(v)){
+                    neighbours.add(v);
+                }
+            }
+        }
+        return adjacencyLists;
+    }
+
 
     private void detectNaturalCuts(){
 
