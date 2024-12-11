75bb52921871c4b6eca049f36cf8c9773c212081
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-12-11 18:28:25 -0300

    feat: add jackson dependency
    Co-authored-by: Pablo Villar <PabVM@users.noreply.github.com>

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module bp.roadnetworkpartitioning {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
+    requires com.fasterxml.jackson.dataformat.xml;
     opens bp.roadnetworkpartitioning to javafx.fxml;
     exports bp.roadnetworkpartitioning;
 }
