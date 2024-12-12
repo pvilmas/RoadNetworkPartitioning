df9d29e8ea46e27aed4bc2b62bcdc080b02c2ccf
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-12-12 19:21:49 -0300

    feat: add to_graph

--- src/main/java/bp/roadnetworkpartitioning/xmlparser/XMLGraph.java
+++ src/main/java/bp/roadnetworkpartitioning/xmlparser/XMLGraph.java
@@ -2,8 +2,8 @@ package bp.roadnetworkpartitioning.xmlparser;
 
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
 import bp.roadnetworkpartitioning.Edge;
+import bp.roadnetworkpartitioning.Vertex;
 import bp.roadnetworkpartitioning.Graph;
 
 @JsonRootName(value = "net")
@@ -40,4 +41,36 @@ public class XMLGraph {
     @JacksonXmlElementWrapper(useWrapping = false)
     public List<NetJunction> junction;
 
+    private Map<String, Vertex> junction_ids = new HashMap<>();
+    private Map<String, Edge> edges_ids = new HashMap<>();
+
+    public Graph to_graph() {
+        Map<Integer, Edge> edges = new HashMap<>();
+        Map<Integer, Vertex> vertices = new HashMap<>();
+        int vertex_id = 0;
+        for(NetJunction junction : this.junction){
+            String id = junction.id;
+            double x = junction.x;
+            double y = junction.y;
+            Vertex vertex = new Vertex(vertex_id, x, y);
+            this.junction_ids.put(id, vertex);
+            vertices.put(vertex_id, vertex);
+            vertex_id++;
+        }
+        int edge_id = 0;
+        for(NetEdge edge : this.edge){
+            if(edge.function != "internal"){
+                String id = edge.id;
+                Vertex from = junction_ids.get(edge.from);
+                Vertex to = junction_ids.get(edge.to);
+                double length = edge.lane.get(0).length;
+                Edge ed = new Edge(edge_id, from, to, length);
+                this.edges_ids.put(id, ed);
+                edges.put(edge_id, ed);
+                edge_id++;
+            }
+        }
+        Graph graph = new Graph(vertices, edges);
+        return graph;
+    }
 }
