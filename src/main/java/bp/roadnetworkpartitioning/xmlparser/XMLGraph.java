9075e05515906375b50f7305ed85c828a6e57cd2
PuntitOwO <christopher.marin@ug.uchile.cl>
2024-12-15 16:28:43 -0300

    feat: add export partitions to XML option

--- src/main/java/bp/roadnetworkpartitioning/xmlparser/XMLGraph.java
+++ src/main/java/bp/roadnetworkpartitioning/xmlparser/XMLGraph.java
@@ -2,11 +2,12 @@ package bp.roadnetworkpartitioning.xmlparser;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -81,5 +82,90 @@ public class XMLGraph {
         Graph graph = new Graph(vertices, edges);
         graph.set_xml_properties(this.version, this.limitTurnSpeed, this.xsi, this.noNamespaceSchemaLocation);
         return graph;
+    }
+
+
+    public void set_junctions(List<NetJunction> junctions){
+        this.junction = junctions;
+    }
+
+    public void set_edges(List<NetEdge> edges){
+        this.edge = edges;
+    }
+
+    public String find_junction_id(Vertex vertex){
+        List<String> result = this.junction_ids
+        .entrySet()
+        .stream()
+        .filter(e -> Objects.equals(e.getValue(), vertex))
+        .map(Map.Entry::getKey).collect(Collectors.toList());
+
+        return result.get(0);
+    }
+
+    public String find_netEdge_id(Edge edge){
+        List<String> result = this.edges_ids
+        .entrySet()
+        .stream()
+        .filter(e -> Objects.equals(e.getValue(), edge))
+        .map(Map.Entry::getKey).collect(Collectors.toList());
+
+        return result.get(0);
+    }
+
+    public NetJunction find_junction(String junction_id){
+        NetJunction result = null;
+        for(NetJunction junction : this.junction){
+            if(junction.id.equals(junction_id)){
+                result = junction;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public NetEdge find_netEdge(String netEdge_id){
+        NetEdge result = null;
+        for(NetEdge netEdge : this.edge){
+            if(netEdge.id.equals(netEdge_id)){
+                result = netEdge;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public XMLGraph to_XmlGraph(Graph graph) {
+        List<NetJunction> junctions = new ArrayList<>();
+        List <NetEdge> netEdges = new ArrayList<>();
+
+        Map<Integer, Vertex> vertices = graph.getVertices();
+        Map<Integer, Edge> edges = graph.getEdges();
+
+        for(Vertex vertex : vertices.values()){
+            String junction_id = find_junction_id(vertex);
+            NetJunction junction = find_junction(junction_id);
+            junctions.add(junction);
+        }
+
+        for(Edge edge : edges.values()){
+            String edge_id = find_netEdge_id(edge);
+            NetEdge ed = find_netEdge(edge_id);
+            netEdges.add(ed);
+        }
+
+        XMLGraph xmlGraph = new XMLGraph();
+        xmlGraph.version = this.version;
+        xmlGraph.junctionCornerDetail = this.junctionCornerDetail;
+        xmlGraph.limitTurnSpeed = this.limitTurnSpeed;
+        xmlGraph.xsi = this.xsi;
+        xmlGraph.noNamespaceSchemaLocation = this.noNamespaceSchemaLocation;
+        xmlGraph.location = this.location;
+        xmlGraph.type = this.type;
+
+        xmlGraph.junction = junctions;
+        xmlGraph.edge = netEdges;
+
+        return xmlGraph;
     }
 }
